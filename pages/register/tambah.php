16c774148c12ae279a7db48862e7fc8888f80a3d
kadabengarann <rio27dh@gmail.com>
2021-12-09 23:09:42 +0800

    password validation fix

--- pages/register/tambah.php
+++ pages/register/tambah.php
@@ -10,38 +10,42 @@ if (isset($_POST["add_record"])) {
     $name = $_POST['name'];
     $uname = $_POST['username'];
     $password = $_POST['password'];
+    $repassword = $_POST['re-password'];
     $gender = $_POST['gender'];
+    if ($password != $repassword) {
+        echo "<script>alert('Password berbeda!!');history.back()</script>";
+    }
 
     $sql_id = "SELECT username FROM user WHERE username='$uname'";
-	$DB->query($sql_id);
-	$DB->execute();
-	$result = $DB->rowCount();
-    if($result > 0){
+    $DB->query($sql_id);
+    $DB->execute();
+    $result = $DB->rowCount();
+    if ($result > 0) {
         echo "<script>alert('Username $uname sudah ada');history.back()</script>";
-    }else{
+    } else {
         $query = "INSERT INTO user (nama, username, password, gender) VALUES (:name,:username,:password,:gender)";
-    $DB->query($query);
-    $DB->bind('name', $name);
-    $DB->bind('username', $uname);
-    $DB->bind('password', $password);
-    $DB->bind('gender', $gender);
-    $DB->execute();
-    $row = $DB->rowCount();
+        $DB->query($query);
+        $DB->bind('name', $name);
+        $DB->bind('username', $uname);
+        $DB->bind('password', $password);
+        $DB->bind('gender', $gender);
+        $DB->execute();
+        $row = $DB->rowCount();
 
-    $query = "SELECT * FROM  user WHERE username= :usname AND password= :pass";
-    $DB->query($query);
-    $DB->bind('usname', $uname);
-    $DB->bind('pass', $password);
-    $DB->execute();
-    $userdata = $DB->single();
+        $query = "SELECT * FROM  user WHERE username= :usname AND password= :pass";
+        $DB->query($query);
+        $DB->bind('usname', $uname);
+        $DB->bind('pass', $password);
+        $DB->execute();
+        $userdata = $DB->single();
 
-    if ($row > 0) {
-        $_SESSION['uname'] = $uname;
-        $_SESSION['id_user'] = $userdata['id_user'];
-        $_SESSION['gender_user'] = $userdata['gender'];
+        if ($row > 0) {
+            $_SESSION['uname'] = $uname;
+            $_SESSION['id_user'] = $userdata['id_user'];
+            $_SESSION['gender_user'] = $userdata['gender'];
 
-        header('Location: ' . base_url);
-    }
+            header('Location: ' . base_url);
+        }
     }
     // $sql = "INSERT INTO regist (nama, username, password, gender,) VALUES (:name,:username,:password,:gender)";
     // $stmt = $pdo_conn->prepare($sql);
